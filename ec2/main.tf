13bd8e88b53deaae5130ba4557603a0c6c78922a
Ram2603 <rambondre9793@gmail.com>
2025-03-17 10:34:06 +0530

    v.4

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,6 @@
 
 module "ec2" {
-  source          = "git@github.com:Ram2603/Stakeholder_390844780898.git//ec2?ref=v.3" 
+  source          = "git@github.com:Ram2603/Stakeholder_390844780898.git//ec2?ref=v.4" 
   instance_type   = "t2.micro"
   aws_region      = "ap-south-1"
 }
