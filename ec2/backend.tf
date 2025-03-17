ac16dd9c9d1ceabb80547b8bf8b2815a9fd8fe3d
Ram2603 <rambondre9793@gmail.com>
2025-03-17 09:54:23 +0530

    new add

--- ec2/backend.tf
+++ ec2/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "aws-stackholder-533267238276"
+    bucket = "aws-stackholder-390844780898"
     key    = "dev-terraform.tfstate"
     region = "ap-south-1"
   }
--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,6 @@
 
 module "ec2" {
-  source          = "git@github.com:satyam88/aws_module_march_2025.git//modules/ec2?ref=v4.0.0"
+  source          = "git@github.com:satyam88/aws_module_march_2025.git//ec2?ref=v.2"
   instance_type   = "t2.micro"
   aws_region      = "ap-south-1"
 }
